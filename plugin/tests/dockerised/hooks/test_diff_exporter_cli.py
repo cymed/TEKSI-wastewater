6a2e2d5e2602b5d46e1b469cee79f2514768c4e2
cymed <102035587+cymed@users.noreply.github.com>
2026-08-31 16:22:01 +0200

    Update test_diff_exporter_cli.py

--- plugin/tests/dockerised/hooks/test_diff_exporter_cli.py
+++ plugin/tests/dockerised/hooks/test_diff_exporter_cli.py
@@ -110,7 +110,7 @@ def run_import_cli(
 
     run_cli(
         command,
-        "/usr/src/plugin/hooks/cli/importer_exporter.py"
+        "/usr/src/plugin/teksi_wastewater/hooks/cli/importer_exporter.py"
     )
 
 
@@ -138,7 +138,7 @@ def run_interlis_import(
 
     run_cli(
         command,
-        "/usr/src/plugin/hooks/cli/importer_exporter.py"
+        "/usr/src/plugin/hooks/cli/teksi_wastewater/importer_exporter.py"
     )
 
 
